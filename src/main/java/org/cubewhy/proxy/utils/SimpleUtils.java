5c834cfca7ec3f04e84345473c4329a70d156728
CubeWhy <duck_chenmy1903@163.com>
2023-10-01 14:19:38 +0800

    multi-domain to same target

--- src/main/java/org/cubewhy/proxy/utils/SimpleUtils.java
+++ src/main/java/org/cubewhy/proxy/utils/SimpleUtils.java
@@ -3,11 +3,9 @@ package org.cubewhy.proxy.utils;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -19,14 +17,29 @@ public class SimpleUtils {
     @Resource
     FileUtils utils;
 
+    private JSONObject config;
+    private JSONObject matches;
+
     public String getTargetHost(String host) throws IOException {
-        String jsonString = readAll(utils.getExternalFile(configFile));
-        JSONObject json = JSON.parseObject(jsonString);
-        JSONObject matches = json.getJSONObject("matches");
+        if (config == null) {
+            String jsonString = readAll(utils.getExternalFile(configFile));
+            config = JSON.parseObject(jsonString);
+            matches = patchMatches(config.getJSONObject("matches"));
+        }
         if (matches.containsKey(host)) {
             return matches.getString(host);
         }
         return null;
+    }
+
+    private JSONObject patchMatches(JSONObject raw) {
+        JSONObject json = new JSONObject();
+        for (String s : raw.keySet()) {
+            for (String domain: s.replace(" ", "").split(",")) {
+                json.put(domain, raw.getString(s));
+            }
+        }
+        return json;
     }
 
 
